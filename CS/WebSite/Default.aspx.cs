0196168a494bd2ee57c8908016c13b0065f80bc0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) { }
+    protected void ASPxGridView1_HtmlRowPrepared(object sender, DevExpress.Web.ASPxGridView.ASPxGridViewTableRowEventArgs e) {
+        e.Row.Attributes.Add("onclick", "onRowClick(" + e.VisibleIndex + ")");
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# ASPxGridView - How to implement a focusable preview row
+
+
+<p>This example demonstrates how to implement a focusable preview row. The JQuery function retrieves the focused row by a CSS class and then changes the class of the next sibling element. The HtmlRowPrepared event is used to add a new attribute for the preview row that allows setting the focused row.</p>
+
+<br/>
+
+
